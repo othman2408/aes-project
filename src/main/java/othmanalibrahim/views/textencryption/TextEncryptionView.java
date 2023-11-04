d6ea44a7f601adcbf6f1d0416ecfe07b29fcc7ea
Othman Alibrahim <acerxr@gmail.com>
2023-11-04 10:33:36 +0300

    Text Encryption View

--- src/main/java/othmanalibrahim/views/textencryption/TextEncryptionView.java
+++ src/main/java/othmanalibrahim/views/textencryption/TextEncryptionView.java
@@ -2,6 +2,9 @@ package othmanalibrahim.views.textencryption;
 
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -15,21 +18,48 @@ import othmanalibrahim.views.MainLayout;
 @RouteAlias(value = "", layout = MainLayout.class)
 public class TextEncryptionView extends HorizontalLayout {
 
-    private TextField name;
-    private Button sayHello;
-
     public TextEncryptionView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
+
+        // vertical layout
+        setAlignItems(Alignment.CENTER);
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setWidthFull();
+        setHeightFull();
+
+        Div mainContainer = new Div();
+        mainContainer.addClassNames("display-flex", "flex-direction-column", "align-items-center", "justify-content-center");
+        mainContainer.setHeight("20rem");
+
+
+        // title
+        H1 title = new H1("Text Encryption");
+
+        //Div
+        Div container = new Div();
+        container.addClassNames("display-flex", "flex-direction-column", "align-items-center", "justify-content-center");
+
+        //add text field
+        TextField textField = new TextField();
+        textField.setPlaceholder("Enter text to encrypt");
+        textField.setClearButtonVisible(true);
+        textField.focus();
+
+        //add button
+        Button button = new Button("Encrypt");
+        button.addClickShortcut(Key.ENTER);
+        button.addClickListener(click -> {
+            Notification.show("Text encrypted");
         });
-        sayHello.addClickShortcut(Key.ENTER);
 
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+        container.add(textField, button);
 
-        add(name, sayHello);
+        mainContainer.add(title, container);
+
+        //add components to layout
+        add(mainContainer);
+
+
+
     }
 
 }
