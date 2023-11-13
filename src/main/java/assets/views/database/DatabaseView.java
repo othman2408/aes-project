349f8f028735fcce9442a1da61398b06fe3e27e4
othman <acerxr@gmail.com>
2023-11-13 07:13:26 +0300

    Update DatabaseView.java

--- src/main/java/assets/views/database/DatabaseView.java
+++ src/main/java/assets/views/database/DatabaseView.java
@@ -8,12 +8,16 @@ import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.dependency.Uses;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
@@ -31,36 +35,86 @@ import static assets.DB.DBConnector.handleRegistration;
 public class DatabaseView extends Composite<VerticalLayout> {
 
     public DatabaseView() {
-        VerticalLayout layout = new VerticalLayout();
-        layout.setAlignItems(Alignment.CENTER);
-        layout.setJustifyContentMode(JustifyContentMode.CENTER);
-        layout.getStyle().set("min-height", "100vh");
+        // Center the content
+        getContent().setAlignItems(Alignment.CENTER);
+        getContent().getStyle().set("min-height", "100vh");
 
-        H3 h3 = new H3("Register Form | Database View");
+
+        // tabs
+        Tab formTab = new Tab("Register Form");
+        Tab recordsTab = new Tab("Database Records");
+        Tabs tabs = new Tabs(formTab, recordsTab);
+        getContent().add(tabs);
+
+        // Content Container
+        VerticalLayout content = new VerticalLayout();
+        content.setAlignItems(Alignment.CENTER);
+        content.setJustifyContentMode(JustifyContentMode.CENTER);
+        content.getStyle().set("min-height", "80vh").set("background-color", "var(--lumo-shade-10pct)").set("border-radius", "var(--lumo-border-radius-m)");
+        getContent().add(content);
+
+        // Set the default tab
+        content.removeAll();
+        content.add(createFormLayout());
+
+
+
+
+
+        // on click on tab from the tabs
+        tabs.addSelectedChangeListener(event -> {
+            if (event.getSelectedTab() == formTab) {
+                content.removeAll();
+                content.add(createFormLayout());
+            } else if (event.getSelectedTab() == recordsTab) {
+                content.removeAll();
+                content.add(new Span("Records"));
+            }
+        });
+
+    }
+
+    private VerticalLayout createFormLayout() {
+        VerticalLayout layout = new VerticalLayout();
+        layout.setAlignItems(FlexComponent.Alignment.CENTER);
+        layout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+
+        H3 h3 = new H3("Register Form");
 
         // Form
-        FormLayout form = new FormLayout();
-        TextField fname = new TextField("First Name");
-        fname.setRequired(true);
-        TextField lname = new TextField("Last Name");
-        lname.setRequired(true);
-        EmailField email = new EmailField("Email");
-        email.setRequired(true);
-        PasswordField pass = new PasswordField("Password field");
-        pass.setRequired(true);
-        pass.setWidth("min-content");
-        DatePicker date = new DatePicker("Birthday");
-        date.setRequired(true);
-        TextField phone = new TextField("Phone Number");
-        phone.setRequired(true);
-        TextField job = new TextField("Occupation");
-        job.setRequired(true);
-
-        //Append all fields to the form
-        form.add(fname, lname, email, pass, date, phone, job);
+        FormLayout form = createForm();
         form.setWidth("100%");
 
         // Buttons
+        HorizontalLayout layoutRow = createButtonLayout();
+
+        // Layout setup
+        layout.setWidth("100%");
+        layout.setMaxWidth("800px");
+        h3.setWidth("100%");
+        layout.add(h3, form, layoutRow);
+
+        return layout;
+    }
+
+    private FormLayout createForm() {
+        FormLayout form = new FormLayout();
+        TextField fname = new TextField("First Name");
+        TextField lname = new TextField("Last Name");
+        EmailField email = new EmailField("Email");
+        PasswordField pass = new PasswordField("Password field");
+        pass.setWidth("min-content");
+        DatePicker date = new DatePicker("Birthday");
+        TextField phone = new TextField("Phone Number");
+        TextField job = new TextField("Occupation");
+
+        // Append all fields to the form
+        form.add(fname, lname, email, pass, date, phone, job);
+
+        return form;
+    }
+
+    private HorizontalLayout createButtonLayout() {
         HorizontalLayout layoutRow = new HorizontalLayout();
         Button buttonPrimary = new Button("Submit");
         Button buttonSecondary = new Button("Clear");
@@ -72,22 +126,10 @@ public class DatabaseView extends Composite<VerticalLayout> {
         layoutRow.getStyle().set("flex-grow", "1");
         layoutRow.add(buttonPrimary, buttonSecondary);
 
-        // Layout setup
-        getContent().setWidth("100%");
-        getContent().getStyle().set("flex-grow", "1");
-        getContent().setJustifyContentMode(JustifyContentMode.START);
-        getContent().setAlignItems(Alignment.CENTER);
-        layout.setWidth("100%");
-        layout.setMaxWidth("800px");
-        layout.setHeight("min-content");
-        h3.setWidth("100%");
-        layout.add(h3, form, layoutRow);
-        getContent().add(layout);
-
-        handleSubmit(buttonPrimary, form);
+        return layoutRow;
     }
 
-    private boolean handleSubmit(Button submitButton, FormLayout form ) {
+    private boolean handleSubmit(Button submitButton, FormLayout form) {
         submitButton.addClickListener(e -> {
             // Get values from the form
             String fname = ((TextField) form.getChildren().toArray()[0]).getValue();
@@ -106,7 +148,6 @@ public class DatabaseView extends Composite<VerticalLayout> {
             Notification.show(phone);
             Notification.show(job);
 
-
             try {
 
                 boolean register = handleRegistration(fname, lname, email, pass, date, phone, job);
@@ -123,6 +164,5 @@ public class DatabaseView extends Composite<VerticalLayout> {
 
         return false;
     }
-
 
 }
